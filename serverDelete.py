94d614ecde223362407433770039242f16766cc3
milenatteixeira <milenateixeira648@gmail.com>
2021-10-25 21:32:18 -0300

    fixed errors

--- serverDelete.py
+++ serverDelete.py
@@ -2,12 +2,17 @@ import socket
 import os
 
 def delete(request):
+    dir_path = ''
+    envia = ''
+    file = ''
+    
     arquivos = os.listdir(dir_path)
     
     if len(arquivos) == 0:
         envia +="Pasta vazia."
         
-    else os.path.exists(dir_path/file)
+    else:
+        os.path.exists(dir_path/file)
         os.remove(dir_path/file)
     
     return 'delete'
