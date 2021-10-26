7a9e4fc23b95c5d3814f0a52828f35aafd8057b0
Camylla Lima Dias <37374749+xcalidi@users.noreply.github.com>
2021-10-25 21:21:10 -0300

    Create serverDelete.py

---
+++ serverDelete.py
@@ -0,0 +1,13 @@
+import socket
+import os
+
+def delete(request):
+    arquivos = os.listdir(dir_path)
+    
+    if len(arquivos) == 0:
+        envia +="Pasta vazia."
+        
+    else os.path.exists(dir_path/file)
+        os.remove(dir_path/file)
+    
+    return 'delete'
